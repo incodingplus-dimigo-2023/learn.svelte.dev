4e31875dee276288631bd97ef11a674f7288b683
myrlagksruf <myrlagksruf@gmail.com>
2023-01-30 13:36:14 +0900

    build 문제 해결 : ?home=<숙제> -> ~<숙제>

--- src/routes/tutorial/[slug]/+page.server.js
+++ src/routes/tutorial/[slug]/+page.server.js
@@ -3,17 +3,16 @@ import { get_homework } from '$lib/server/homework';
 import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
-export function load({ params, url }) {
-	let homeUrl = url.searchParams.get('home');
-
+export function load({ params }) {
 	/**@type {import('$lib/types').Exercise | undefined} */
 	let exercise;
+	let slugs = params.slug.split('~');
 	let isHome = false;
-	if(homeUrl){
+	if(slugs[1]){
 		isHome = true;
-		exercise = get_homework(params.slug, homeUrl);
+		exercise = get_homework(slugs[0], slugs[1]);
 	} else {
-		exercise = get_exercise(params.slug);
+		exercise = get_exercise(slugs[0]);
 	}
 	if (!exercise) {
 		throw error(404, 'No such tutorial found');
