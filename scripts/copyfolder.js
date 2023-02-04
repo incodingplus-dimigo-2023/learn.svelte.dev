403feb9d5e9fdbd06821b3f9b4102ea148309a00
myrlagksruf <myrlagksruf@gmail.com>
2023-02-04 14:40:04 +0900

    homework 다운로드 script 추가

--- scripts/copyfolder.js
+++ scripts/copyfolder.js
@@ -1,12 +1,33 @@
 import glob from 'glob';
-import path from 'path/posix';
+import path from 'path';
 import { fileURLToPath } from 'url';
-console.log(pro)
-console.log(process.argv[2])
+import fs from 'fs';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const o = path.parse('.');
-const p = path.resolve(__dirname, '..','content_kor/tutorial');
-console.log(o, p);
-// glob(`${p}\\`, (e, arr) => {
-//     console.log(arr);
-// })
+const dest = process.argv[2];
+if(!fs.existsSync(dest)){
+    throw Error('존재하지 않는 경로');
+}
+const rel = path.relative(path.resolve('./'), path.resolve(__dirname, '../content_kor/tutorial'));
+const root = `./${rel.replaceAll(path.sep, path.posix.sep)}`;
+glob(`${root}/**/home/*/app-a/`, (e, arr) => {
+    const dir = fs.readdirSync(dest, {withFileTypes:true});
+    for(let i of dir){
+        if(i.name.startsWith('.')) continue;
+        if(!i.isDirectory()) continue;
+        fs.rmSync(path.resolve(dest, i.name), {
+            force:true,
+            recursive:true,
+        });
+    }
+    for(let i of arr){
+        const curDest = path.resolve(dest, i.replace(root, '.'), '..');
+        const resArr = curDest.split(path.sep);
+        let index = resArr.lastIndexOf('home');
+        if(index === -1) continue;
+        resArr.splice(index, 1);
+        fs.cpSync(i, resArr.join(path.sep), {
+            force:true,
+            recursive:true,
+        });
+    }
+})
