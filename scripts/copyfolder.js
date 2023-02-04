ee353f22ff7a3e8f4b4c4373af6a0f56d5d1cf81
myrlagksruf <myrlagksruf@gmail.com>
2023-02-05 00:41:08 +0900

    webcontainer 업데이트

--- scripts/copyfolder.js
+++ scripts/copyfolder.js
@@ -2,27 +2,43 @@ import glob from 'glob';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
+import { promisify } from 'util';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const dest = process.argv[2];
+
 if(!fs.existsSync(dest)){
     throw Error('존재하지 않는 경로');
 }
 const rel = path.relative(path.resolve('./'), path.resolve(__dirname, '../content_kor/tutorial'));
 const root = `./${rel.replaceAll(path.sep, path.posix.sep)}`;
-glob(`${root}/**/home/*/app-a/`, (e, arr) => {
-    fs.rmSync(path.resolve(dest, 'src'), {
+const proGlob = promisify(glob);
+const appA = await proGlob(`${root}/**/home/*/app-a/`);
+// const common = await proGlob(`${root}/**/common/`);
+
+fs.rmSync(path.resolve(dest, 'src'), {
+    force:true,
+    recursive:true,
+});
+for(let i of appA){
+    const curDest = path.resolve(dest, 'src', i.replace(root, '.'));
+    const resArr = curDest.split(path.sep);
+    let index = resArr.lastIndexOf('home');
+    if(index === -1) continue;
+    resArr.splice(index, 1);
+    fs.cpSync(i, resArr.join(path.sep), {
         force:true,
         recursive:true,
     });
-    for(let i of arr){
-        const curDest = path.resolve(dest, 'src', i.replace(root, '.'));
-        const resArr = curDest.split(path.sep);
-        let index = resArr.lastIndexOf('home');
-        if(index === -1) continue;
-        resArr.splice(index, 1);
-        fs.cpSync(i, resArr.join(path.sep), {
-            force:true,
-            recursive:true,
-        });
-    }
-})
+    fs.cpSync(i, path.resolve(resArr.join(path.sep), '../app-b'), {
+        force:true,
+        recursive:true,
+    });
+}
+
+// for(let i of common){
+//     const curDest = path.resolve(dest, 'content', i.replace(root, '.'));
+//     fs.cpSync(i, curDest, {
+//         force:true,
+//         recursive:true,
+//     });
+// }
