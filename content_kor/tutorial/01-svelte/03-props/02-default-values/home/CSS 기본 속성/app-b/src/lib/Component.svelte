53ebc9448769b8f2e552e8aed76d3fd699a81476
UNWHO <eunhoo0713@gmail.com>
2023-02-18 22:00:20 +0900

    01-03 props 숙제 추가

---
+++ content_kor/tutorial/01-svelte/03-props/02-default-values/home/CSS 기본 속성/app-b/src/lib/Component.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let backgroundColor = "transparent"
+    export let color = "canvastext"
+    export let padding = "0"
+</script>
+
+<div class="container">
+    <div>
+        <div>background-color: {backgroundColor};</div>
+        <div>color: {color};</div>
+        <div>padding: {padding};</div>
+    </div>
+    <div class="view" style={`background-color: ${backgroundColor}; color: ${color}; padding: ${padding}`}>텍스트</div>
+</div>
+
+<style>
+    .container{
+        display: flex;
+        padding: 30px;
+    }
+    .view {
+        width: 100px;
+        height: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-left: 30px;
+    }
+</style>
---
+++ content_kor/tutorial/01-svelte/03-props/03-spread-props/home/성적표/app-b/src/lib/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Component from "./Component.svelte";
+
+	let arr = [{
+		name: "김철수",
+		korean: 89,
+		math: 97,
+		english: 78
+	}, {
+		name: "김영희",
+		korean: 64,
+		math: 58,
+		english: 88
+	}, {
+		name: "홍길동",
+		korean: 92,
+		math: 84,
+		english: 100
+	}]
+</script>
+
+<Component name={arr[0].name} korean={arr[0].korean} math={arr[0].math} english={arr[0].english} />
+<Component name={arr[1].name} korean={arr[1].korean} math={arr[1].math} english={arr[1].english} />
+<Component name={arr[2].name} korean={arr[2].korean} math={arr[2].math} english={arr[2].english} />
