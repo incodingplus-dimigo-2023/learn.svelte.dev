5cb45640fa957f8169eeddb66c3d5ad93aed6d7e
myrlagksruf <myrlagksruf@gmail.com>
2023-02-23 17:56:26 +0900

    teacher header 확인

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,6 +2,7 @@ export const prerender = true;
 
 /** @type {import('./$types').LayoutServerLoad} */
 export const load = ({request}) => {
+    console.log(request.headers.get('teacher'), typeof request.headers.get('teacher'));
 	let isTeacher = request.headers.get('teacher') === 'true' ? true : false;
     return {
         isTeacher
