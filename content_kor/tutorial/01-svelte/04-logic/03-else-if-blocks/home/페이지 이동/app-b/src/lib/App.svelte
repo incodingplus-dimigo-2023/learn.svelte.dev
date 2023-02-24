9939fc8b8f409c5570ddb629a7599e2ea46ea7bf
myrlagksruf <myrlagksruf@gmail.com>
2023-02-24 12:40:43 +0900

    페이지 이동 숙제 수정

--- content_kor/tutorial/01-svelte/04-logic/03-else-if-blocks/home/페이지 이동/app-b/src/lib/App.svelte
+++ content_kor/tutorial/01-svelte/04-logic/03-else-if-blocks/home/페이지 이동/app-b/src/lib/App.svelte
@@ -20,14 +20,16 @@
 	<h1>마지막 페이지입니다.</h1>
 	<div>감사합니다.</div>
 </div>
-{:else}
-<div>
-	<h1>잘못된 페이지입니다.</h1>
-	<button on:click={() => page = 1}>1 페이지로 돌아가기</button>
-</div>
 {/if}
 
-<div>
-	<button on:click={() => page--}>이전 페이지로</button>
-	<button on:click={() => page++}>다음 페이지로</button>
-</div>
+{#if page >= 1 && page <= 3}
+	<div>
+		<button on:click={() => page--}>이전 페이지로</button>
+		<button on:click={() => page++}>다음 페이지로</button>
+	</div>
+{:else}
+	<div>
+		<h1>잘못된 페이지입니다.</h1>
+		<button on:click={() => page = 1}>1 페이지로 돌아가기</button>
+	</div>
+{/if}
