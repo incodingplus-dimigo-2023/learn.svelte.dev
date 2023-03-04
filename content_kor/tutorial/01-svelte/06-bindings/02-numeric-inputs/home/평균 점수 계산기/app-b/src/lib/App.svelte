7aa87bf456470acaa521115ab617491fc0dc06f5
UNWHO <eunhoo0713@gmail.com>
2023-03-05 07:44:14 +0900

    01-06 bindings 숙제 추가

---
+++ content_kor/tutorial/01-svelte/06-bindings/02-numeric-inputs/home/평균 점수 계산기/app-b/src/lib/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	const MAX_SCORE = 100
+	
+	let subjects = [{
+		name: "국어",
+		score: 0
+	}, {
+		name: "수학",
+		score: 0
+	}, {
+		name: "영어",
+		score: 0
+	}]
+
+	let averageScore = 0
+	$: averageScore = subjects.reduce((acc, subject) => acc += subject.score, 0) / subjects.length
+</script>
+
+<div>
+	{#each subjects as subject}
+		<div>
+			<span>{subject.name}</span>
+			<input type="number" min="0" max={MAX_SCORE} bind:value={subject.score}>
+			<span>점</span>
+		</div>	
+	{/each}
+	<div>평균 {averageScore}점</div>
+</div>
+
+
+
---
+++ content_kor/tutorial/01-svelte/06-bindings/06-select-bindings/home/학교 학년 선택/app-b/src/lib/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	const schools = ["초등학교", "중학교", "고등학교"]
+
+	let selectedSchool
+	let selectedGrade
+
+	let maxGrade
+	$: maxGrade = selectedSchool === "초등학교" ? 6 : 3
+</script>
+
+<select bind:value={selectedSchool}>
+	{#each schools as school}
+		<option value={school}>{school}</option>
+	{/each}
+</select>
+
+
+<select bind:value={selectedGrade}>
+	{#each new Array(maxGrade) as _, index}
+		<option value={index + 1}>{index + 1}학년</option>
+	{/each}
+</select>
+
+
+<div>{selectedSchool} {selectedGrade}학년</div>
+
