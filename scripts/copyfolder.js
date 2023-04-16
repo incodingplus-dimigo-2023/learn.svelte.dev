40ff8ce44ab668a7af5ed1006a786d0d2e2aa108
myrlagksruf <myrlagksruf@gmail.com>
2023-04-16 18:31:17 +0900

    copyfolder 수정

--- scripts/copyfolder.js
+++ scripts/copyfolder.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import glob from 'glob';
+import { glob } from 'glob';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
